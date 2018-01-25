1384efd256b593f272d7e26d93f72565cf0f6825
Todor Klasnakov <t.klasnakov@gmail.com>
2018-01-25 18:01:57 +0200

    Begin to add functionality for "Problem22-MonthlyCalendar"

---
+++ Javascript Fundamentals Homeworks/Exercises/Exercises-ConditionalStatementAndLoops/Problem22-MonthlyCalendar.js
@@ -0,0 +1,44 @@
+/**
+ * Created by todork on 1/25/2018.
+ */
+
+function createMonthlyCalendar(input) {
+
+    const totalDaysInAWeek = 7;
+    const totalCalendarDays = 35;
+
+    let day = input[0];
+    let month = input[1];
+    let year = input[2];
+    // let date = new Date(year, month - 1, day);
+    let lastMonthLastDay = new Date(year, month - 1, 0).getDate();
+    let lastMonthLastDayOfTheWeek = new Date(year, month - 1, 0).getDay();
+    let lastMonthLastSunday = new Date(year, month - 1, 0 - lastMonthLastDayOfTheWeek).getDate();
+    let lastDayOfCurrentMonth = new Date(year, month, 0).getDate();
+    let daysInTheLastWeek = lastMonthLastDay - lastMonthLastSunday + 1;
+    let daysInTheWeekCounter = 0;
+    let totalDaysCounter = 0;
+
+
+    let calendar = `<table>\n<tr><th>Sun</th><th>Mon</th><th>Tue</th><th>Wed</th><th>Thu</th><th>Fri</th><th>Sat</th></tr>\n`;
+
+    if (lastMonthLastDayOfTheWeek !== 6) {
+        addDaysFromLastMonth()
+    }
+
+    function addDaysFromLastMonth() {
+        calendar += `<tr>`;
+        for (let i = 0; i < daysInTheLastWeek; i++) {
+            calendar += `<td class="prev-month">${lastMonthLastSunday + i}</td>`;
+        }
+    }
+
+    for (let i = 0; i < lastDayOfCurrentMonth; i++) {
+
+    }
+
+
+    console.log(calendar)
+}
+
+createMonthlyCalendar([24, 1, 2018]);
