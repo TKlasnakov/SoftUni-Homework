378bb5c9001a3bb3fae4cfa7258e7c07428b8858
TKlas <t.klasnakov@gmail.com>
2018-01-25 23:06:28 +0200

    Finalize Problem22-MonthlyCalendar

--- Javascript Fundamentals Homeworks/Exercises/Exercises-ConditionalStatementAndLoops/Problem22-MonthlyCalendar.js
+++ Javascript Fundamentals Homeworks/Exercises/Exercises-ConditionalStatementAndLoops/Problem22-MonthlyCalendar.js
@@ -3,43 +3,107 @@
  */
 
 function createMonthlyCalendar(input) {
-
     const totalDaysInAWeek = 7;
-    const totalCalendarDays = 35;
 
     let day = input[0];
     let month = input[1];
     let year = input[2];
-    // let date = new Date(year, month - 1, day);
+    let today = new Date(year, month - 1, day).getDate();
     let lastMonthLastDay = new Date(year, month - 1, 0).getDate();
     let lastMonthLastDayOfTheWeek = new Date(year, month - 1, 0).getDay();
     let lastMonthLastSunday = new Date(year, month - 1, 0 - lastMonthLastDayOfTheWeek).getDate();
     let lastDayOfCurrentMonth = new Date(year, month, 0).getDate();
     let daysInTheLastWeek = lastMonthLastDay - lastMonthLastSunday + 1;
+    let currentMonthCounter = 0;
     let daysInTheWeekCounter = 0;
     let totalDaysCounter = 0;
 
-
     let calendar = `<table>\n<tr><th>Sun</th><th>Mon</th><th>Tue</th><th>Wed</th><th>Thu</th><th>Fri</th><th>Sat</th></tr>\n`;
 
-    if (lastMonthLastDayOfTheWeek !== 6) {
-        addDaysFromLastMonth()
+    function activate() {
+        if (lastMonthLastDayOfTheWeek !== 6) {
+            addDaysFromLastMonth();
+            addDaysForTheFirstWeek();
+        }
+        addDaysForTheCurrentMonth();
+        addDaysForTheNexMonth()
     }
 
+    activate();
+
+
     function addDaysFromLastMonth() {
-        calendar += `<tr>`;
+        calendar += `  <tr>`;
         for (let i = 0; i < daysInTheLastWeek; i++) {
             calendar += `<td class="prev-month">${lastMonthLastSunday + i}</td>`;
             daysInTheWeekCounter ++;
+            totalDaysCounter++;
         }
     }
 
-    for (let i = 0; i < lastDayOfCurrentMonth; i++) {
-
+    function addDaysForTheFirstWeek() {
+        let currentMonthFirstWeekDays = totalDaysInAWeek - daysInTheWeekCounter;
+        for (let i = 0; i < currentMonthFirstWeekDays; i++) {
+            if (today - 1 === i) {
+                calendar += `<td class="today">${i + 1}</td>`;
+                daysInTheWeekCounter ++;
+                totalDaysCounter++;
+                currentMonthCounter ++;
+            } else {
+                calendar += `<td>${i + 1}</td>`;
+                daysInTheWeekCounter ++;
+                totalDaysCounter++;
+                currentMonthCounter ++;
+            }
+        }
+        daysInTheWeekCounter = 0;
+        calendar += `</tr>\n`;
     }
 
+    function addDaysForTheCurrentMonth() {
+        calendar += `  <tr>`;
+        for (let i = currentMonthCounter; i < lastDayOfCurrentMonth; i++) {
+            if (daysInTheWeekCounter === 6 && i !== lastDayOfCurrentMonth - 1) {
+                if (today - 1 === i) {
+                    calendar += `<td class="today">${currentMonthCounter + 1}</td></tr>\n  <tr>`;
+                    daysInTheWeekCounter = 0;
+                    totalDaysCounter ++;
+                    currentMonthCounter ++;
+                } else {
+                    calendar += `<td>${currentMonthCounter + 1}</td></tr>\n  <tr>`;
+                    daysInTheWeekCounter = 0;
+                    totalDaysCounter ++;
+                    currentMonthCounter ++;
+                }
+            } else {
+                if (today - 1 === i) {
+                    calendar += `<td class="today">${currentMonthCounter + 1}</td>`;
+                    daysInTheWeekCounter ++;
+                    totalDaysCounter ++;
+                    currentMonthCounter ++;
+                } else {
+                    calendar += `<td>${currentMonthCounter + 1}</td>`;
+                    daysInTheWeekCounter ++;
+                    totalDaysCounter ++;
+                    currentMonthCounter ++;
+                }
+            }
+        }
+    }
 
-    console.log(daysInTheWeekCounter)
+    function addDaysForTheNexMonth() {
+        let daysFromNextMonth = totalDaysInAWeek - daysInTheWeekCounter;
+        if (daysFromNextMonth !== 7) {
+            for (let i = 0; i < daysFromNextMonth; i++) {
+                calendar += `<td class="next-month">${i + 1}</td>`
+            }
+            calendar += `</tr>`
+        }
+    }
+
+    calendar += `\n</table>`;
+
+    return calendar;
 }
 
-createMonthlyCalendar([24, 1, 2018]);
+createMonthlyCalendar([12, 7, 1986]);
